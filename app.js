a04c96b1b3113aff78c39c9dd55ca3fd22dd6437
CristianSAM <elementrixz512@gmail.com>
2018-03-26 17:03:22 -0500

    Agregando API con Express

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({extended: false}))
 
 const places = [
   {
@@ -17,10 +21,19 @@ const places = [
     'description': 'Descripción óptima',
     'address': '0123091203'
   },
+  {
+    'title': 'Oficina retórica',
+    'description': 'Descripción óptima',
+    'address': '0123091203'
+  },
 
 ];
 app.get('/', (req,res)=>{
     res.json(places);
+});
+
+app.post('/',(req,res)=>{
+   res.json(req.body.nombre);
 });
 
 app.use(express.static('public'));
