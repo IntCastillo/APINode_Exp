d7bbe7fec2eabf9f1d523df17c7d993dc244bc0f
CristianSAM <elementrixz512@gmail.com>
2018-03-26 16:16:32 -0500

    Datos JSON

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
+const places = [
+  {
+     'title': 'Oficina retórica',
+     'description': 'Descripción óptima',
+     'address': '0123091203' 
+  },
+  {
+    'title': 'Oficina retórica',
+    'description': 'Descripción óptima',
+    'address': '0123091203'
+  },
+  {
+    'title': 'Oficina retórica',
+    'description': 'Descripción óptima',
+    'address': '0123091203'
+  },
+
+];
 app.get('/', (req,res)=>{
-    res.send('Hey, Sr!')
+    res.json(places);
 });
 
 app.use(express.static('public'));
